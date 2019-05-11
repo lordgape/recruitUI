a81daeb3214f28f8a30cf256837a6dc91903dc9d
Chesvic Esuani <lordgape@gmail.com>
2019-05-11 14:10:37 +0100

    Apply functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,47 @@ import {JobserviceService} from "./jobservice.service";
 export class AppComponent {
   title = 'Adio Consultancy';
 
-  constructor(private _jobService: JobserviceService) {
+  passportToUpload: File = null;
+  resumeToUpload: File = null;
+
+  constructor(private jobService: JobserviceService) {
 
   }
 
-  handleFileInput(files: FileList) {
+  handlePassportInput(files: FileList) {
 
+    this.passportToUpload = files.item(0);
+
+    let allowedTypes = ['image/jpeg'];
+
+    this.canSubmit = allowedTypes.indexOf(this.fileToUpload.type) >= 0;
+
+
+
+  }
+
+
+
+  handleResumeInput(files: FileList) {
+
+    this.resumeToUpload = files.item(0);
+
+
+  }
+
+  applyForRole() {
+
+
+    let firstname =  $("#first_name").val();
+
+    let surname =  $("#surname").val();
+
+    let email =  $("#email").val();
+
+    let phone =  $("#phone").val();
+
+    let cover_letter =  $("#cover_letter").val();
+
+    this.jobService.apply(firstname, surname, email, phone, cover_letter, this.passportToUpload, this.resumeToUpload);
   }
 }
