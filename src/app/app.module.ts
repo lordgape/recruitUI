b27d801744eea9ff07cf20ed5c1f55055bba3845
Chesvic Esuani <lordgape@gmail.com>
2019-05-11 20:07:47 +0100

    Import missing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {JobserviceService} from "./jobservice.service";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpClientModule, HttpHandler} from "@angular/common/http";
 
 
 @NgModule({
@@ -12,7 +12,8 @@ import {HttpClient} from "@angular/common/http";
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [JobserviceService, HttpClient],
   bootstrap: [AppComponent]
